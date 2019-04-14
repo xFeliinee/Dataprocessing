80619a6229731ba967a052ec624e86d1cf59a9da
Feline Benavides <feline.benavides@student.uva.nl>
2019-04-14 17:12:24 +0200

    pushje

--- Homework/Week2/eda.py
+++ Homework/Week2/eda.py
@@ -1,6 +1,20 @@
-# Helloworld
+#!/usr/bin/env python
+# Name: Feline Benavides
+# Student number: 11035358
+"""
+This script does something :)
+"""
+
 # pandas geïntstalleerd
 
 import pandas
 
 # bij kopje parsing
+
+# Load in the CSV file?
+# Clean and preprocess the data?
+# Compute AND print the mean, median and mode of the GDP data?
+# Produce a histogram of the GDP data?
+# Compute AND print the Five Number Summary of the Infant Mortality data?
+# Produce a box plot of the Infant Mortality data?
+# Write a .json file in the correct format?
