5d165f30fc73ce8d043615a39c79af13ea5bd279
Feline Benavides <feline.benavides@student.uva.nl>
2019-04-15 15:32:44 +0200

    Bijna klaar voor week 2 :)

--- Homework/Week2/eda.py
+++ Homework/Week2/eda.py
@@ -7,6 +7,7 @@ This script does something :)
 Thanks to:
 https://www.datacamp.com/community/tutorials/pandas-read-csv
 All pandas videos of https://www.youtube.com/user/DrNoureddinSadawi/videos
+pandas 0.24.2 documentation
 
 Set index shit:
 https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.set_index.html
@@ -20,46 +21,52 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-        # Load in CSV file
+        # Load in CSV file and set Country as first column
         df = pd.read_csv("input.csv", na_values = "unknown", decimal = ",")
         df = df.set_index("Country")
 
         # Compute and print the mean, median and mode of the GDP data
         df["GDP ($ per capita) dollars"] = df["GDP ($ per capita) dollars"].str.replace("dollars", "")
         df["GDP ($ per capita) dollars"] = pd.to_numeric(df["GDP ($ per capita) dollars"], downcast = "float")
-        # print(df["GDP ($ per capita) dollars"].mean())
-        # print(df["GDP ($ per capita) dollars"].median())
-        # print(df["GDP ($ per capita) dollars"].mode())
+        GDP_mean = df["GDP ($ per capita) dollars"].mean()
+        GDP_median = df["GDP ($ per capita) dollars"].median()
+        GDP_mode = int(df["GDP ($ per capita) dollars"].mode())
+        # print(f"For the Gross Domestic Product (GDP) per capita in dollar the mean is {GDP_mean}, the median is {GDP_median} and the mode is {GDP_mode}.")
 
         # Produce a histogram of the GDP data
-        ax = df["GDP ($ per capita) dollars"].plot.hist()
-        plt.show()
+        df["GDP ($ per capita) dollars"].plot.hist()
+        plt.title('Gross Domestic Product (GDP) per capita', \
+                    color = 'g', fontsize = 14)
+        plt.ylabel('Frequency', color = 'g')
+        plt.xlabel('Dollars', color = 'g')
+        # plt.show()
 
         # Compute and print the Five Number Summary of the Infant Mortality data
+        min = df["Infant mortality (per 1000 births)"].min()
+        max = df["Infant mortality (per 1000 births)"].max()
+        median = df["Infant mortality (per 1000 births)"].median()
+        quantile_25 = df["Infant mortality (per 1000 births)"].quantile(0.25)
+        quantile_75 = df["Infant mortality (per 1000 births)"].quantile(0.75)
+        # print(f"The minimum for the Infant Mortality data is {min} and maximum is {max}. The median is {median}, the 25% quantile is {quantile_25} and the 75% quantile is {quantile_75}.")
 
+        # Produce a box plot of the Infant Mortality data
+        df.boxplot(["Infant mortality (per 1000 births)"])
+        plt.title('Box plot for infant mortality', \
+                    color = 'g', fontsize = 14)
+        plt.ylabel('Infant mortality', color = 'g')
+        plt.xlabel('')
+        # plt.show()
 
-# V Load in the CSV file?
-# V (think so) Clean and preprocess the data?
-# V Compute AND print the mean, median and mode of the GDP data?
-# V Produce a histogram of the GDP data?
-# Compute AND print the Five Number Summary of the Infant Mortality data?
-# Produce a box plot of the Infant Mortality data?
-# Write a .json file in the correct format?
-
-
+"""
+    TODO
+V Load in the CSV file?
+V (think so) Clean and preprocess the data?
+V Compute AND print the mean, median and mode of the GDP data?
+V Produce a histogram of the GDP data?
+V Compute AND print the Five Number Summary of the Infant Mortality data?
+V Produce a box plot of the Infant Mortality data?
+Write a .json file in the correct format?
+"""
+        # Write a .json file in the correct format
         data = df.to_json("hello.json", orient = "index")
-
         #.set_index(Region)?
-        # print(df.head())
-        # print(data)
-        print("Gerund!")
-        # print(data.head(5))
-        # print(data[["Population"]].mean())
-        # dubbele brackets: 1 voor PY, 1 voor pd (selecting column)
-        # check = data[["Agriculture"]].replace(' ', np.NaN)
-        # print(sum(check.isnull().sum()))
-        # print(data['Agriculture'])
-        # print(statistics.mean(data['Agriculture']))
-
-        # Central tendency
-        # median, modus and mean
