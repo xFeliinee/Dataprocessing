79e2ed462b3ba2ee6c52de8dedca8e94bd0439d2
Feline Benavides <feline.benavides@student.uva.nl>
2019-04-30 13:50:30 +0200

    push

--- Homework/Week3/chart.js
+++ Homework/Week3/chart.js
@@ -4,6 +4,25 @@
 * Purpose of this file:
 **/
 
+// Vaag huissie van de site
+const canvas = document.getElementById('my-house');
+const ctx = canvas.getContext('2d');
+// Set line width
+ctx.lineWidth = 10;
+
+// Wall
+ctx.strokeRect(75, 140, 150, 110);
+
+// Door
+ctx.fillRect(130, 190, 40, 60);
+
+// Roof
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath();
+ctx.stroke();
+
 // Loading the data, snippet from minor programming
 var fileName = "KNMI_data.json";
 var txtFile = new XMLHttpRequest();
@@ -43,26 +62,6 @@ function createTransform(domain, range){
     }
 }
 
-
-
-// Vaag huissie van de site
-const canvas = document.getElementById('my-house');
-const ctx = canvas.getContext('2d');
-// Set line width
-ctx.lineWidth = 10;
-
-// Wall
-ctx.strokeRect(75, 140, 150, 110);
-
-// Door
-ctx.fillRect(130, 190, 40, 60);
-
-// Roof
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
 // Transforming the data (if necessary)
 
 // Plotting a line chart with JavaScript. Do not use any external libraries!
