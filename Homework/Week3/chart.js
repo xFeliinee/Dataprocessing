fb48fb8a76a050dcc54623caa6a981304b5afea6
Feline Benavides <feline.benavides@student.uva.nl>
2019-04-30 12:34:33 +0200

    push

--- Homework/Week3/chart.js
+++ Homework/Week3/chart.js
@@ -1,5 +1,20 @@
-// comment
+/*
+Name: Feline Benavides
+Student number: 11035358
+Purpose of this file:
+*/
 
-Loading the data
-Transforming the data (if necessary)
-Plotting a line chart with JavaScript. Do not use any external libraries!
+// Loading the data, snippet from minor programming
+var fileName = "KNMI_data.json";
+var txtFile = new XMLHttpRequest();
+txtFile.onreadystatechange = function() {
+    if (txtFile.readyState === 4 && txtFile.status == 200) {
+        console.log(JSON.parse(txtFile.responseText));
+    }
+}
+txtFile.open("GET", fileName);
+txtFile.send();
+
+// Transforming the data (if necessary)
+
+// Plotting a line chart with JavaScript. Do not use any external libraries!
