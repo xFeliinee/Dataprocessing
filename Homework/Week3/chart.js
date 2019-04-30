b24882f31a2d5abe3d231d01d8734d452906798f
Feline Benavides <feline.benavides@student.uva.nl>
2019-04-30 13:44:01 +0200

    push

--- Homework/Week3/chart.js
+++ Homework/Week3/chart.js
@@ -1,45 +1,46 @@
-/*
-Name: Feline Benavides
-Student number: 11035358
-Purpose of this file:
-*/
+/**
+* Name: Feline Benavides
+* Student number: 11035358
+* Purpose of this file:
+**/
 
 // Loading the data, snippet from minor programming
 var fileName = "KNMI_data.json";
 var txtFile = new XMLHttpRequest();
 txtFile.onreadystatechange = function() {
     if (txtFile.readyState === 4 && txtFile.status == 200) {
-        console.log(JSON.parse(txtFile.responseText));
+        var data =JSON.parse(txtFile.responseText);
+        data.forEach(function(data){
+            console.log(data);
+        }        
     }
 }
 txtFile.open("GET", fileName);
 txtFile.send();
 
-txtFile.forEach(function(element) {
-  console.log(element);
-  console.log("hallo wereld")
-});
 
-console.log("boe")
+/**
+* Purpose of this function
+**/
+function createTransform(domain, range){
+	// domain is a two-element array of the data bounds [domain_min, domain_max]
+	// range is a two-element array of the screen bounds [range_min, range_max]
+	// this gives you two equations to solve:
+	// range_min = alpha * domain_min + beta
+	// range_max = alpha * domain_max + beta
+ 		// a solution would be:
 
-// Vaag huissie van de site
-const canvas = document.getElementById('my-house');
-const ctx = canvas.getContext('2d');
-// Set line width
-ctx.lineWidth = 10;
+    var domain_min = domain[0]
+    var domain_max = domain[1]
+    var range_min = range[0]
+    var range_max = range[1]
 
-// Wall
-ctx.strokeRect(75, 140, 150, 110);
+    // formulas to calculate the alpha and the beta
+   	var alpha = (range_max - range_min) / (domain_max - domain_min)
+    var beta = range_max - alpha * domain_max
 
-// Door
-ctx.fillRect(130, 190, 40, 60);
-
-// Roof
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
-// Transforming the data (if necessary)
-
-// Plotting a line chart with JavaScript. Do not use any external libraries!
+    // returns the function for the linear transformation (y= a * x + b)
+    return function(x){
+      return alpha * x + beta;
+    }
+}
