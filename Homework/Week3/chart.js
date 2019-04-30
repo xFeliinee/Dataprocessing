1edffeb45bd74756a1e8cad88d7cd2b6d571afd5
Feline Benavides <feline.benavides@student.uva.nl>
2019-04-30 14:14:18 +0200

    push

--- Homework/Week3/chart.js
+++ Homework/Week3/chart.js
@@ -4,28 +4,28 @@
 * Purpose of this file:
 **/
 
-// // Vaag huissie van de site
-// const canvas = document.getElementById('chart');
-// const ctx = canvas.getContext('2d');
-// // Set line width
-// ctx.lineWidth = 5;
-//
-// yScale = (canvas.height - columnSize - margin) / (Val_max - Val_min);
-// xScale = (canvas.width - rowSize) / sections;
-// // Wall
-// ctx.strokeRect(75, 140, 150, 110);
-//
-// // Door
-// ctx.fillRect(130, 190, 40, 60);
-//
-// // Roof
-// ctx.moveTo(50, 140);
-// ctx.lineTo(150, 60);
-// ctx.lineTo(250, 140);
-// ctx.closePath();
-// ctx.stroke();
-//
-// // Loading the data, snippet from minor programming
+// Vaag huissie van de site
+const canvas = document.getElementById('chart');
+const ctx = canvas.getContext('2d');
+// Set line width
+ctx.lineWidth = 5;
+
+yScale = (canvas.height - columnSize - margin) / (Val_max - Val_min);
+xScale = (canvas.width - rowSize) / sections;
+// Wall
+ctx.strokeRect(75, 140, 150, 110);
+
+// Door
+ctx.fillRect(130, 190, 40, 60);
+
+// Roof
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath();
+ctx.stroke();
+
+// Loading the data, snippet from minor programming
 var fileName = "KNMI_data.json";
 var txtFile = new XMLHttpRequest();
 txtFile.onreadystatechange = function() {
