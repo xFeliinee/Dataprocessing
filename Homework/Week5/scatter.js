bf264546198ce3cf67169b87cd2f83b844878153
Feline Benavides <feline.benavides@student.uva.nl>
2019-05-08 17:01:20 +0200

    save the changes!

--- Homework/Week5/scatter.js
+++ Homework/Week5/scatter.js
@@ -1,1 +1,12 @@
-// Hallo :)
+/**
+* Name: Feline Benavides
+* Student number: 11035358
+* This file does something...
+**/
+
+// alles moet nu in functions
+
+window.onload = function() {
+
+  console.log('Yes, you can!')
+};
