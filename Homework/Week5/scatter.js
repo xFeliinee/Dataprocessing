3f963c1cd5b2ed623f9b563345792f61dfe84012
Feline Benavides <feline.benavides@student.uva.nl>
2019-05-15 18:07:29 +0200

    huiswerk week 5

--- Homework/Week5/scatter.js
+++ Homework/Week5/scatter.js
@@ -1,10 +1,10 @@
 /**
 * Name: Feline Benavides
 * Student number: 11035358
-* This file does something...
+* This files makes scatterplot with given data for selected year
+*
 **/
 
-// alles moet nu in functions
 
 window.onload = function() {
      // Getting die aapies
@@ -16,101 +16,119 @@ window.onload = function() {
     var requests = [d3.json(teensInViolentArea), d3.json(teenPregnancies), d3.json(GDP)];
 
     Promise.all(requests).then(function(response) {
-        var cleanTeenViolent = transformResponse(response[0])
-        var cleanTeenPregnancies = transformResponse(response[1])
-        var cleanGDP = transformResponse2(response[2])
-        // console.log(cleanTeenPregnancies);
-        scatterPlot(cleanGDP, cleanTeenViolent, cleanTeenPregnancies)
+        var dataSet = loadData(response)
+        scatterPlot(dataSet, 2012)
+        document.getElementById("2012").onclick = function(){
+            d3.selectAll("svg > *").remove()
+            scatterPlot(dataSet, 2012)
+        }
+        document.getElementById("2013").onclick = function(){
+            d3.selectAll("svg > *").remove()
+            scatterPlot(dataSet, 2013)
+        }
+        document.getElementById("2014").onclick = function(){
+            d3.selectAll("svg > *").remove()
+            scatterPlot(dataSet, 2014)
+        }
+        document.getElementById("2015").onclick = function(){
+            d3.selectAll("svg > *").remove()
+            scatterPlot(dataSet, 2015)
+        }
     }).catch(function(e){
         throw(e);
     });
+};
 
-    console.log('Kakzooi')
 
+/* This function cleans the given data */
+function loadData(response) {
+    var cleanTeenViolent = transformResponse(response[0])
+    var cleanTeenPregnancies = transformResponse(response[1])
+    var cleanGDP = transformResponse2(response[2])
+    var dataSet = {2012 : [], 2013 : [], 2014 : [], 2015 : []};
+
+    Object.keys(cleanTeenViolent).forEach(function(d) {
+        cleanTeenViolent[d].forEach(function(e) {
+            if (dataSet[e.Time]) {
+                if(!(e.Country == "OECD - Average")){
+                    dataSet[e.Time].push({
+                        "Country": e.Country,
+                        "teenViolent": e.Datapoint
+                    })
+                }
+            }
+        })
+    })
+
+    Object.keys(cleanGDP).forEach(function(d) {
+        cleanGDP[d].forEach(function(e) {
+            if (dataSet[e.Year]) {
+                dataSet[e.Year].forEach(function(f){
+                    if (f.Country == e.Country) {
+                        f["GDP"] = e.Datapoint
+                    }
+                })
+            }
+        })
+    })
+
+    Object.keys(cleanTeenPregnancies).forEach(function(d) {
+        cleanTeenPregnancies[d].forEach(function(e) {
+            if (dataSet[e.Time]) {
+                dataSet[e.Time].forEach(function(f){
+                    if (f["Country"] == e.Country) {
+                        f["teenPregnancies"] = e.Datapoint
+                    }
+                })
+            }
+        })
+    })
+    return dataSet
 };
 
 
 /*
-* This function does something
+* This function creates a scatterplot for given data
 */
-function scatterPlot(xValues, yValues, zValues){
-    // Define height and width
+function scatterPlot(data, year){
+    // Define margins, height and width
     var margin = {top: 50, right: 200, bottom: 50, left: 50};
-    let w = 1000;
-    let h = 700;
-    let plotWidth = 1000 - margin.left - margin.right;
-    let plotHeight = 700 - margin.bottom - margin.top;
+    var w = 1000;
+    var h = 700;
+    var plotWidth = 1000 - margin.left - margin.right;
+    var plotHeight = 700 - margin.bottom - margin.top;
+
+    var y = year;
+    var ymax = xmax = zmax = -Infinity;
+    data[y].forEach(function (d){
+        if (d.GDP > xmax) {
+            xmax = d.GDP
+        }
+        if (d.teenViolent > ymax) {
+            ymax = d.teenViolent
+        }
+        if (d.teenPregnancies > zmax) {
+            zmax = d.teenPregnancies
+        }
+    })
+
+    // Set scales for axis
+    var yScale = d3.scaleLinear()
+                    .domain([2, ymax])
+                    .range([plotHeight, 0]);
+
+    var xScale = d3.scaleLinear()
+                    .domain([10000, xmax])
+                    .range([0, plotWidth]);
+
+    var zScale = d3.scaleThreshold()
+                    .domain([(1/4 * zmax), (2/4 * zmax), (3/4 * zmax), zmax])
+                    .range(['#ffffb2', '#fecc5c', '#fd8d3c', '#e31a1c']);
 
     // Getting DOM element for chart
     var scatter = d3.select("svg")
 
-    let y = 2012;
-    ykeys = Object.keys(yValues);
-    let ymin = xmin = zmin = Infinity;
-    let ymax = xmax = zmax = -Infinity;
-    for (var i = 0; i < ykeys.length; i++) {
-        for (var j = 0; j < yValues[ykeys[i]].length; j++) {
-            try {
-              xValues[ykeys[i]][j].Datapoint;
-            }
-            catch(error) {
-              continue;
-            }
-            if (yValues[ykeys[i]][j].Datapoint == false || xValues[ykeys[i]][j].Datapoint == false) {
-                continue;
-            } else {
-                if (yValues[ykeys[i]][j].Datapoint > ymax) {
-                    ymax = yValues[ykeys[i]][j].Datapoint;
-                }
-                if (yValues[ykeys[i]][j].Datapoint < ymin) {
-                    ymin = yValues[ykeys[i]][j].Datapoint;
-                }
-                if (xValues[ykeys[i]][j].Datapoint > xmax) {
-                    xmax = xValues[ykeys[i]][j].Datapoint
-                }
-                if (xValues[ykeys[i]][j].Datapoint < xmin) {
-                    xmin = xValues[ykeys[i]][j].Datapoint
-                }
-            }
-        }
-    };
-
-
-    // making an ordinal scale
-    for (var i = 0; i < ykeys.length; i++) {
-        for (var j = 0; j < yValues[ykeys[i]].length; j++) {
-            try {
-              zValues[ykeys[i]][j].Datapoint;
-            }
-            catch(error) {
-              continue;
-            }
-            if (zValues[ykeys[i]][j].Datapoint == false) {
-                continue;
-            } else {
-                if (zValues[ykeys[i]][j].Datapoint > zmax) {
-                    zmax = zValues[ykeys[i]][j].Datapoint;
-                }
-                if (zValues[ykeys[i]][j].Datapoint < zmin) {
-                    zmin = zValues[ykeys[i]][j].Datapoint;
-                }
-            }
-        }
-    };
-    var zScale = d3.scaleOrdinal()
-                    .domain([zmin, zmax])
-                    .range(["#ffeda0", "#feb24c", "#f03b20"]);
-
-
-    // Set scales for axis
-    var yScale = d3.scaleLinear()
-                    .domain([ymin, ymax])
-                    .range([plotHeight, 0]);
-
-    var xScale = d3.scaleLinear()
-                    .domain([xmin, xmax])
-                    .range([0, plotWidth]);
-
+    // Getting axis
     scatter.attr("width", w)
             .attr("height", h)
             .append("g")
@@ -124,7 +142,6 @@ function scatterPlot(xValues, yValues, zValues){
                                 (plotHeight + margin.top) + ")")
             .call(d3.axisBottom(xScale));
 
-
     // Set titles to axis
     scatter.append("text")
             .attr("x", ((plotHeight / 2) + margin.top)*-1)
@@ -132,7 +149,7 @@ function scatterPlot(xValues, yValues, zValues){
             .attr("transform", "rotate(-90)")
             .attr("text-anchor", "middle")
             .style("font-size", "20px")
-            .text("Number of teens in a violent area");
+            .text("Percentage of teens in a violent area");
 
     scatter.append("text")
             .attr("x", plotWidth / 2 + margin.left)
@@ -143,11 +160,10 @@ function scatterPlot(xValues, yValues, zValues){
 
     scatter.append("text")
             .attr("x", plotWidth / 2 + margin.left)
-            .attr("y", margin.top / 3)
+            .attr("y", margin.top / 2)
             .attr("text-anchor", "middle")
-            .style("font-size", "20px")
-            .text("Grote titel");
-
+            .style("font-size", "25px")
+            .text("Scatterplot of teens in violent areas vs GDP");
 
     // Getting the tip
     var tip = d3.tip()
@@ -155,44 +171,27 @@ function scatterPlot(xValues, yValues, zValues){
                 .offset([-10, 0])
                 .html(function(d) {
                     return "<b>Country:</b> <span style='color:orange'>" +
-                            yValues[d][0].Country + "</span>";
+                            d.Country + "</span>" + "<br/>" +
+                            "<b>Teen Pregnancy (%):</b>" +
+                            " <span style='color:orange'>" + d.teenPregnancies +
+                            "</span>";
                  });
     scatter.call(tip);
 
-
     // Creating dots
     scatter.selectAll("circle")
-            .data(ykeys)
+            .data(data[y])
             .enter()
             .append("circle")
-            .attr("cx", function(d, i){
-                return i*50
-            }) //function(d) {
-            //     if (xValues[d]) {
-            //         xValues[d].forEach(function(e) {
-            //             if (e.Year == y){
-            //                 // console.log((xScale(e.Datapoint)) + margin.left);
-            //                 return (xScale(e.Datapoint)) + margin.left
-            //             }
-            //         })
-            //     }
-            // })
-            .attr("cy", function(d, i){
-                return i*50
+            .attr("cx", function(d) {
+                return xScale(d.GDP) + margin.left
             })
-            //function(d) {
-            //     if (yValues[d]) {
-            //         yValues[d].forEach(function(e) {
-            //             if (e.Time == y) {
-            //                 // console.log((yScale(e.Datapoint)) + margin.top);
-            //                 return h - (yScale(e.Datapoint)) + margin.top
-            //             }
-            //         })
-            //     }
-            // })
+            .attr("cy", function(d) {
+                return yScale(d.teenViolent) + margin.top
+            })
             .attr("r", 5)
             .attr("fill", function(d) {
-                return zScale(d)
+                return zScale(d.teenPregnancies)
             })
             .on("mouseover", tip.show)
             .on("mouseenter", function(d){
@@ -203,33 +202,25 @@ function scatterPlot(xValues, yValues, zValues){
             .on("mouseout", function(d){
                 d3.select(this)
                     .attr("fill", function(d) {
-                        return zScale(d)
-                    })
+                        return zScale(d.teenPregnancies)
+                    });
             });
 
 
-    // Creating legend
-    scatter.selectAll("rect")
-            .data(zValues)
-            .enter()
-            .append("rect")
-
-    // scatter.selectAll("legend")
-    //         .data(1)
-    //         .enter()
-    //         .append("legend")
-    //         .attr("x", h - margin.right)
-    //         .attr("y", margin.top)
-    //         .attr("height", plotHeight)
-    //         .attr("width", margin.right - 100)
-    //         .attr("stroke", "black")
-    //         .attr("fill", "white")
-    //         .attr("rx", 10)
-    //         .attr("ry", 10)
-    //         .append("text")
-    //                 .text("Legandary");
+// // creating a legend
+//     var legend = scatter.append("g")
+//                         .attr("class","legend")
+//
+//     legend.append("border")
+//             .attr("x", w - margin.right)
+//             .attr("y", margin.top)
+//             .attr("width", 150)
+//             .attr("height", 200)
+//             .attr("stroke", "orange")
+//             .attr("fill", "white")
+//             .attr("rx", 10)
+//             .attr("ry", 10);
 };
-
 
 
 /********
@@ -381,7 +372,7 @@ function transformResponse2(data){
 };
 
 
-/* When the user clicks on the button, 
+/* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
