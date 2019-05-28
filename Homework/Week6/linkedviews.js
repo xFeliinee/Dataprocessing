69ff01a55fe0d87fd8e1bb27de03ffb739459492
Feline Benavides <feline.benavides@student.uva.nl>
2019-05-28 17:13:10 +0200

    Klaar met data processing :)

--- Homework/Week6/linkedviews.js
+++ Homework/Week6/linkedviews.js
@@ -7,35 +7,35 @@
 **/
 
 
-d3v5.json("HPI_data.json").then(function(data) {
+d3v5.json("HPI_data.json").then(function (data) {
+    "use strict";
     // making averages for barchart
-    var sumExpectancy = 0;
-    var sumHappyYears = 0;
-    var sumInequality = 0;
+    var sumExpectancy, sumHappyYears, sumInequality;
+    sumExpectancy = sumHappyYears = sumInequality = 0;
 
-    Object.keys(data).forEach(function(d) {
-        sumExpectancy += data[d]["Average Life Expectancy"]
-        sumInequality += data[d]["Inequality-adjusted Life Expectancy"]
-        sumHappyYears += data[d]["Happy Life Years"]
+    Object.keys(data).forEach(function (d) {
+        sumExpectancy += data[d]["Average Life Expectancy"];
+        sumInequality += data[d]["Inequality-adjusted Life Expectancy"];
+        sumHappyYears += data[d]["Happy Life Years"];
     });
 
     var length = Object.keys(data).length;
     var averages = [];
-    averages.push((Math.round((sumExpectancy / length) * 10 ) / 10),
-                  (Math.round((sumInequality / length) * 10 ) / 10),
-                  (Math.round((sumHappyYears / length) * 10 ) / 10));
+    averages.push((Math.round((sumExpectancy / length) * 10) / 10),
+                  (Math.round((sumInequality / length) * 10) / 10),
+                  (Math.round((sumHappyYears / length) * 10) / 10));
 
     // Getting the maximum and minimum of the Happy Planet Index
     var fillKey = "fillKey";
     var max = -Infinity;
     var min = Infinity;
 
-    Object.keys(data).forEach(function(d) {
+    Object.keys(data).forEach(function (d) {
         if (data[d]["Happy Planet Index"] > max) {
-            max = data[d]["Happy Planet Index"]
+            max = data[d]["Happy Planet Index"];
         }
         if (data[d]["Happy Planet Index"] < min) {
-            min = data[d]["Happy Planet Index"]
+            min = data[d]["Happy Planet Index"];
         }
     });
 
@@ -43,13 +43,13 @@ d3v5.json("HPI_data.json").then(function(data) {
 
     // Making a colorscale based on the Happy Planet Index
     var colorScale = d3v5.scaleThreshold()
-                        .domain([(1/3 * colorRange + min),
-                                 (2/3 * colorRange + min), colorRange + min])
+                        .domain([(1 / 3 * colorRange + min),
+                                 (2 / 3 * colorRange + min), colorRange + min])
                         .range(["Low", "Medium", "High"]);
 
     // Add the color to the .json file
-    Object.keys(data).forEach(function(d) {
-        var value = colorScale(data[d]["Happy Planet Index"])
+    Object.keys(data).forEach(function (d) {
+        var value = colorScale(data[d]["Happy Planet Index"]);
         data[d][fillKey] = value;
     });
 
@@ -68,7 +68,7 @@ d3v5.json("HPI_data.json").then(function(data) {
             highlightFillColor: "orange",
             highlightBorderColor: "rgba(0, 0, 0, 0.4)",
             highlightBorderWidth: 2,
-            popupTemplate: function(geography, d) {
+            popupTemplate: function (geography, d) {
                 // If the country exist in the data give ranking
                 if (d) {
                     return ["<div class='hoverinfo'><b>Country: </b>",
@@ -76,39 +76,38 @@ d3v5.json("HPI_data.json").then(function(data) {
                            "</b>", d["HPIRank"], "</br><b>Happy Planet ",
                            "Index-value: </b>", d["Happy Planet Index"],
                            "</div>"].join('');
-                } else {
-                    return ["<div class='hoverinfo'><b>Country: </b>",
+                }
+                return ["<div class='hoverinfo'><b>Country: </b>",
                             geography.properties.name, "</br><i>No data",
                             " available from the Happy Planet Index</i>",
                             "</div>"].join('');
-                }
             }
         },
-        done: function(datamap) {
-            datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography, d) {
+        done: function (datamap) {
+            datamap.svg.selectAll('.datamaps-subunit').on('click', function (geography) {
                 // Check for data
                 if (data[geography.id]) {
                     // Make a color library
-                    if (data[geography.id][fillKey] == "Low") {
+                    if (data[geography.id][fillKey] === "Low") {
                         var color = "#f7fcb9";
-                    } else if (data[geography.id][fillKey] == "Medium"){
+                    } else if (data[geography.id][fillKey] === "Medium") {
                         var color = "#addd8e";
                     } else {
                         var color = "#31a354";
                     };
                     // Check for a barchart and update it
                     if (document.getElementById("barchart")) {
-                        updateBarchart(data[geography.id], averages, color)
+                        updateBarchart(data[geography.id], averages, color);
                     }
                 };
-            })
+            });
         }
     });
     // Draw a legend for this map
     map.legend({
         legendTitle : "Range of Happy Planet Index ",
-        defaultFillName: "No data",
-    })
+        defaultFillName: "No data"
+    });
     // Start the page with a barchart about the Netherlands
     barChart(data["NLD"], averages, "#31a354");
 });
@@ -123,7 +122,7 @@ function barChart(dataset, averages, color) {
     d3v5.select("body")
         .append("div")
         .attr("id", "barText")
-        .html(function(d) {
+        .html(function () {
           return ["This barchart shows various variables of the Happy Planet ",
                   "Index in years. Inequality-adjusted Life Expectancy is ",
                   "</br>", "adjusted to inequality of that land. If you hover ",
@@ -174,7 +173,7 @@ function barChart(dataset, averages, color) {
 
     // Set titles to axis
     bars.append("text")
-        .attr("x", ((h / 2) + margin.top)*-1)
+        .attr("x", ((h / 2) + margin.top) * -1)
         .attr("y", margin.left / 3)
         .attr("transform", "rotate(-90)")
         .attr("text-anchor", "middle")
@@ -184,7 +183,7 @@ function barChart(dataset, averages, color) {
     bars.append("text")
         .attr("id", "title")
         .attr("x", w / 2 + margin.left)
-        .attr("y", h + margin.top + ((4/5) * margin.bottom))
+        .attr("y", h + margin.top + ((4 / 5) * margin.bottom))
         .attr("text-anchor", "middle")
         .style("font-size", "20px")
         .text("Data from " + dataset["Country"]);
@@ -197,7 +196,7 @@ function barChart(dataset, averages, color) {
         .text("Variables from the Happy Planet Index");
 
     // Getting the data needed for the bars
-    data = [];
+    var data = [];
     data.push(dataset["Average Life Expectancy"],
               dataset["Inequality-adjusted Life Expectancy"],
               dataset["Happy Life Years"]);
@@ -206,7 +205,7 @@ function barChart(dataset, averages, color) {
     var tip = d3v5.tip()
               .attr("id", "d3Tip")
               .offset([-10, 0])
-              .html(function(d, i) {
+              .html(function (d, i) {
                   return "Exact number: <span style='color:orange'>" + d +
                          "</span><br/>Average: " + "<span style='color:orange'>"
                          + averages[i] + "</span></br>Inequality: " +
@@ -220,27 +219,27 @@ function barChart(dataset, averages, color) {
         .data(data)
         .enter()
         .append("rect")
-        .attr("x", function(d, i) {
+        .attr("x", function (d, i) {
             return (i * (barWidth + barPadding)) + margin.left + barPadding;
         })
-        .attr("y", function(d){
+        .attr("y", function (d) {
             return yScale(d) + margin.top;
         })
-        .attr("height", function(d){
+        .attr("height", function (d) {
             return h - yScale(d);
         })
         .attr("width", barWidth)
         .attr("stroke", "#E4DBE4")
         .attr("fill", color)
         .on("mouseover", tip.show)
-        .on("mouseenter", function(d){
+        .on("mouseenter", function () {
             d3v5.select(this)
-            .attr("fill", "orange")
+                    .attr("fill", "orange");
         })
         .on("mouseleave", tip.hide)
-        .on("mouseout", function(d){
+        .on("mouseout", function () {
             d3v5.select(this)
-                .attr("fill", color)
+                    .attr("fill", color);
         });
 };
 
@@ -249,7 +248,7 @@ function barChart(dataset, averages, color) {
  * This function updates the initial barChart with new data from the land
  * there is clicked on, if there is data from the Happy Planet Index.
  **/
-function updateBarchart(dataset, averages, color){
+function updateBarchart(dataset, averages, color) {
     // Define height and width of the plot
     var margin = {top: 50, right: 10, bottom: 50, left: 50};
     var w = 600;
@@ -263,7 +262,7 @@ function updateBarchart(dataset, averages, color){
                     .range([h, 0]);
 
     // Getting the data needed for the bars
-    data = [];
+    var data = [];
     data.push(dataset["Average Life Expectancy"],
               dataset["Inequality-adjusted Life Expectancy"],
               dataset["Happy Life Years"]);
@@ -274,26 +273,26 @@ function updateBarchart(dataset, averages, color){
                             .exit();
 
     // Select barchart to add new data
-    var update = d3v5.select("#barchart")
+    var update = d3v5.select("#barchart");
 
     // Add a new title
     update.append("text")
             .attr("id", "title")
             .attr("x", w / 2 + margin.left)
-            .attr("y", h + margin.top + ((4/5) * margin.bottom))
+            .attr("y", h + margin.top + ((4 / 5) * margin.bottom))
             .attr("text-anchor", "middle")
             .style("font-size", "20px")
             .text("Data from " + dataset["Country"]);
 
     // Select bars to append new data
     var newBars = update.selectAll("rect")
-                            .data(data)
+                            .data(data);
 
     // Add a new tip
     var tip = d3v5.tip()
               .attr("id", "d3Tip")
               .offset([-10, 0])
-              .html(function(d, i) {
+              .html(function (d, i) {
                   return "Exact number: <span style='color:orange'>" + d +
                          "</span><br/>Average: " + "<span style='color:orange'>"
                          + averages[i] + "</span></br>Inequality: " +
@@ -307,29 +306,29 @@ function updateBarchart(dataset, averages, color){
             .enter()
             .append("rect")
             .attr("class", "enter")
-            .attr("x", function(d, i) {
+            .attr("x", function (d, i) {
                 return (i * (barWidth + barPadding)) +
                         margin.left + barPadding;
             })
             .attr("width", barWidth)
             .attr("stroke", "#E4DBE4")
             .merge(newBars)
-                .attr("y", function(d){
+                .attr("y", function (d) {
                         return yScale(d) + margin.top;
                     })
-                .attr("height", function(d){
+                .attr("height", function (d) {
                         return h - yScale(d);
                     })
                 .attr("fill", color)
                 .on("mouseover", tip.show)
-                .on("mouseenter", function(d, i) {
+                .on("mouseenter", function () {
                     d3v5.select(this)
-                    .attr("fill", "orange")
+                    .attr("fill", "orange");
                 })
                 .on("mouseleave", tip.hide)
-                .on("mouseout", function(d, i) {
+                .on("mouseout", function () {
                     d3v5.select(this)
-                        .attr("fill", color)
+                        .attr("fill", color);
                 });
 
     newBars.exit()
